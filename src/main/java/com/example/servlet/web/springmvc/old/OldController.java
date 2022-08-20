b24fc176ab2d0e62aa4b37d4828efab886dd4a00
mykim <mykim8710@gmail.com>
2022-08-20 20:16:39 +0900

    [ dev ] 핸들러 매핑과 핸들러 어댑터 - OldController @Controller이전 컨트롤러 구현 [ edit ] application.properties - 뷰 리졸버 설정

---
+++ src/main/java/com/example/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,17 @@
+package com.example.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component("/springmvc/old-controller")  // 스프링 빈의 이름으로 URL을 매핑할 것이다.
+public class OldController implements Controller {
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");    //논리적인 이름
+    }
+}
